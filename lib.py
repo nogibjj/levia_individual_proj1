f70af078fd5737b7726307bc64b61dd15705ef3c
Levia Yang <73449544+xxxtesttest@users.noreply.github.com>
2023-09-19 20:52:12 -0400

    Create lib.py

---
+++ lib.py
@@ -0,0 +1,36 @@
+import os
+import polars as pl
+import matplotlib.pyplot as plt
+
+def read_csv(file_path):
+    return pl.read_csv(file_path)
+
+def calculate_statistics(data):
+    selected_columns = ['danceability', 'energy', 'artist_popularity', 'loudness']
+    data = data[selected_columns]
+    return data.describe()
+
+def visualize_data(data, save_path):
+    if not isinstance(data, pl.DataFrame):
+        raise ValueError("Input is not a Polar DataFrame")
+        # Create a directory to store the plots if save_path is provided
+    if save_path:
+        os.makedirs(save_path, exist_ok=True)
+
+        # Iterate over each numeric column and create a histogram
+    histogram_paths = []
+    for col in data.columns:
+        plt.figure(figsize=(8, 6))
+        plt.hist(data[col], bins=20, edgecolor='k', alpha=0.7)
+        plt.xlabel(col)
+        plt.ylabel("Frequency")
+        plt.title(f"Histogram of {col}")
+        plt.grid(True)
+        if save_path:
+            histogram_path = os.path.join(save_path, f"{col}_histogram.png")
+            plt.savefig(histogram_path)
+            plt.close()
+            histogram_paths.append(histogram_path)
+        else:
+            plt.show()
+    return histogram_paths
